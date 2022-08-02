7501271ce750348ce747f43415d645ef2daaff6a
Joo Young Mok <78552207+gloriamok@users.noreply.github.com>
2022-08-02 22:39:04 +0900

    Add files via upload

---
+++ baekjoon_n11722_dp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, arr[1000]{}, length[1000]{};
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        length[i] = 1;
+        for (int j = 0; j < i; j++)
+            if (arr[j] > arr[i])
+                length[i] = max(length[i], length[j] + 1);
+    }
+    cout << *max_element(length, length + n);
+}
