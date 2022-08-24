632312d430799784cba9d11da54761a83fd6655f
Joo Young Mok <78552207+gloriamok@users.noreply.github.com>
2022-08-24 15:53:38 +0900

    Update baekjoon_n11720_Implementation.cpp

--- baekjoon_n11720_Implementation.cpp
+++ baekjoon_n11720_Implementation.cpp
@@ -6,9 +6,9 @@ int main() {
 	int n, sum = 0;
 	cin >> n;
 	char num[100]{};
-	// cout << int(num[i])-48 << " "; // 0~9의 아스키코드는 48~57
-	for (int i = 0; i < n; i++) { 
+	for (int i = 0; i < n; i++) { // 0~9의 아스키코드는 48~57
 		cin >> num[i];
+		// cout << int(num[i])-48 << " ";
 		sum += int(num[i]) - 48;
 	}
 	cout << sum;
